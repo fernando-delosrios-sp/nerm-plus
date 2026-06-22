bc09900b92f7be053a9874044c91e19ded514c26
fernando-delosrios-sp <64795004+fernando-delosrios-sp@users.noreply.github.com>
2026-06-22 04:03:10 +0000

    Refactor strict undefined and null checks to loose null checks

--- src/nerm-client.ts
+++ src/nerm-client.ts
@@ -711,7 +711,7 @@ export class NERMClient {
                             if (childrenProfiles) {
                                 if (Array.isArray(childrenProfiles)) {
                                     isMulti = true
-                                    values = values.concat(childrenProfiles.filter((x) => x !== undefined))
+                                    values = values.concat(childrenProfiles.filter((x) => x != null))
                                 } else {
                                     values.push(childrenProfiles)
                                 }
@@ -725,7 +725,7 @@ export class NERMClient {
                         const referencedProfiles = await Promise.all(
                             profileNames.map((x) => this.resolveProfileByValueOrName(x, attributeType.profile_type_id))
                         )
-                        values = referencedProfiles.filter((x) => x !== undefined)
+                        values = referencedProfiles.filter((x) => x != null)
                     } else {
                         values = [profileAttribute].flat()
                     }
